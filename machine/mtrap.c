425015225b9fee1187f4186d63a7adc7b2146084
yangguang <yangguang@canaan-creative.com>
2022-09-27 15:50:33 +0800

    add sbi call to flush l1 dcache on remote cpu

--- machine/mtrap.c
+++ machine/mtrap.c
@@ -15,6 +15,75 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#define CCTL_L1D_IX_INVAL	16
+#define CCTL_L1D_IX_WB		17
+#define CCTL_L1D_IX_RTAG	19
+
+typedef struct {
+  volatile uint64_t remote_cache_flush_va;
+  volatile uint64_t remote_cache_flush_pa;
+} remote_dcache_op_data_t;
+
+remote_dcache_op_data_t remote_dcache_op_data[2] = {{0,0}, {0,0}};
+
+void ipi_dcache_wb(void)
+{
+
+  int hartid = read_csr(mhartid);
+
+  int i=0;
+  uint64_t tag;
+  uint64_t pa = remote_dcache_op_data[hartid].remote_cache_flush_pa;
+
+  uint64_t org_mcctlbeginaddr = read_csr(mcctlbeginaddr);
+
+  for(i=0; i<4; i++)
+  {
+    write_csr(mcctlbeginaddr, (i<<13)|(pa & (0xffUL<<5)));
+    write_csr(mcctlcommand, CCTL_L1D_IX_RTAG); //read tag
+    tag=read_csr(mcctldata);
+    
+    if( (tag & (1UL<<63)) && ( (pa>>12) == ((tag & 0xffffffffUL)>>2) ) )
+    {
+      write_csr(mcctlbeginaddr, (i<<13)|(pa & (0xffUL<<5)));
+      write_csr(mcctlcommand,  CCTL_L1D_IX_WB); //index wb/inval		
+      break;
+    }
+  }
+
+  write_csr(mcctlbeginaddr, org_mcctlbeginaddr);
+
+}
+
+void ipi_dcache_invalid(void)
+{
+
+  int hartid = read_csr(mhartid);
+
+  int i=0;
+  uint64_t tag;
+  uint64_t pa = remote_dcache_op_data[hartid].remote_cache_flush_pa;
+
+  uint64_t org_mcctlbeginaddr = read_csr(mcctlbeginaddr);
+
+  for(i=0; i<4; i++)
+  {
+    write_csr(mcctlbeginaddr, (i<<13)|(pa & (0xffUL<<5)));
+    write_csr(mcctlcommand, CCTL_L1D_IX_RTAG); //read tag
+    tag=read_csr(mcctldata);
+    
+    if( (tag & (1UL<<63)) && ( (pa>>12) == ((tag & 0xffffffffUL)>>2) ) )
+    {
+      write_csr(mcctlbeginaddr, (i<<13)|(pa & (0xffUL<<5)));
+      write_csr(mcctlcommand,  CCTL_L1D_IX_INVAL); //index wb/inval		
+      break;
+    }
+  }
+
+  write_csr(mcctlbeginaddr, org_mcctlbeginaddr);
+
+}
+
 void __attribute__((noreturn)) bad_trap(uintptr_t* regs, uintptr_t dummy, uintptr_t mepc)
 {
   die("machine mode: unhandlable trap %d @ %p", read_csr(mcause), mepc);
@@ -169,6 +238,45 @@ static void send_ipi_many(uintptr_t* pmask, int event)
   }
 }
 
+static void send_ipi_dcache_flush(uintptr_t mask, int event, uint64_t va, uint64_t pa)
+{
+  remote_dcache_op_data_t* data = (mask == 0x2) ? &remote_dcache_op_data[1] : &remote_dcache_op_data[0];
+
+  data->remote_cache_flush_va = va;
+  data->remote_cache_flush_pa = pa;
+
+//  printm("send_ipi_dcache_flush: 1 , %lx, %lx, %lx, %lx \r\n", mask, event, va, pa);
+  // send IPIs to everyone
+  for (uintptr_t i = 0, m = mask; m; i++, m >>= 1)
+    if (m & 1)
+      send_ipi(i, event);
+
+//  printm("send_ipi_dcache_flush: 2 \r\n");
+  if (event == IPI_SOFT)
+    return;
+
+//  printm("send_ipi_dcache_flush: 3 \r\n");
+  // wait until all events have been handled.
+  // prevent deadlock by consuming incoming IPIs.
+  uint32_t incoming_ipi = 0;
+  for (uintptr_t i = 0, m = mask; m; i++, m >>= 1)
+    if (m & 1)
+      while (plic_sw_get_pending(i)) {
+        plic_sw_claim();
+        if (HLS()->plic_sw.source_id) {
+          incoming_ipi |= 1 << (HLS()->plic_sw.source_id);
+          plic_sw_complete();
+        }
+      }
+
+//  printm("send_ipi_dcache_flush: 4 \r\n");
+  // if we got an IPI, restore it; it will be taken after returning
+  if (incoming_ipi) {
+    *(HLS()->plic_sw.pending) = incoming_ipi;
+    mb();
+  }
+}
+
 uintptr_t mcall_get_cycles(uintptr_t* mtime_val)
 {
     *mtime_val = *mtime;
@@ -201,6 +309,16 @@ void mcall_trap(uintptr_t* regs, uintptr_t mcause, uintptr_t mepc)
       goto send_ipi;
     case SBI_REMOTE_FENCE_I:
       ipi_type = IPI_FENCE_I;
+      goto send_ipi;
+    case SBI_REMOTE_DCACHE_WB:
+      ipi_type = IPI_DCACHE_WB;
+      goto send_ipi_dcache;
+    case SBI_REMOTE_DCACHE_INVALID:
+      ipi_type = IPI_DCACHE_INVALID;
+send_ipi_dcache:
+      send_ipi_dcache_flush(arg0, ipi_type, arg1, arg2);
+      retval = 0;
+      break;
 send_ipi:
       send_ipi_many((uintptr_t*)arg0, ipi_type);
       retval = 0;
